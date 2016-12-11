66ea3c2c2f1a45a1aea68a78af4e78c83f8ed165
solanto <solanto@users.noreply.github.com>
2016-12-11 09:03:59 -0800

    Updated scripts.js

--- purplerain/scripts.js
+++ purplerain/scripts.js
@@ -1,14 +1,30 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var raindrop = {
-  x: canvas.width/2,
-  y: 0,
-  length: 10,
-  width: 2.5,
-  speed: 1
+var rain = [];
+var numberOfRaindrops = 100;
+
+function Raindrop() {
+  this.x = canvas.width/2,
+  this.y = 0,
+  this.length = 10,
+  this.width = this.length/4,
+  this.speed = 1
+}
+
+function populateRain() {
+  for (i = 0; i < numberOfRaindrops; i++) { 
+    rain[i] = new Raindrop;
+  }
 }
 
 function fall() {
-  raindrop.y += raindrop.speed
+  Raindrop.y += Raindrop.speed
 }
 
+function show() {
+  ctx.beginPath();
+  ctx.rect(brickX, brickY, brickWidth, brickHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
