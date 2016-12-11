78d214ce2124c9ee69a0084fa5f23d26ee11aa3f
solanto <solanto@users.noreply.github.com>
2016-12-11 13:28:33 -0800

    Updated scripts.js

--- purplerain/scripts.js
+++ purplerain/scripts.js
@@ -7,8 +7,10 @@ var ctx = canvas.getContext("2d");
 var drops = [];
 var backgroundOpacity = 1 - motionBlurAmount;
 var backgroundColor = window.getComputedStyle(canvas, null).getPropertyValue('background-color');
-canvas.width = window.getComputedStyle(canvas, null).getPropertyValue('width');
-canvas.height = window.getComputedStyle(canvas, null).getPropertyValue('height');
+var canvasWidthFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('width');
+canvas.width = canvasWidthFromCSS.substring(0, canvasWidthFromCSS.length - 2);
+var canvasHeightFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('height');
+canvas.height = canvasHeightFromCSS.substring(0, canvasHeightFromCSS.length - 2);
 
 function random(min, max) {
   return Math.floor((Math.random() * max) + min);
