2c3f72963373af928174c447a4bd271986a08f62
solanto <solanto@users.noreply.github.com>
2016-12-11 21:26:17 -0800

    Updated scripts.js

--- metaballs/scripts.js
+++ metaballs/scripts.js
@@ -4,37 +4,7 @@ var canvasWidthFromCSS = window.getComputedStyle(canvas, null).getPropertyValue(
 canvas.width = canvasWidthFromCSS.substring(0, canvasWidthFromCSS.length - 2);
 var canvasHeightFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('height');
 canvas.height = canvasHeightFromCSS.substring(0, canvasHeightFromCSS.length - 2);
-var pixels = []
-var metaPoints
-
-function random(min, max) {
-  return Math.floor((Math.random() * max) + min);
-}
-
-function Pixel() {
-    this.colorR = random(0, 255);
-    this.colorG = random(0, 255);
-    this.colorB = random(0, 255);
-}
-
-function makePixels() {
-  for(x = 0; x < canvas.width; x++){
-    for(y = 0; y < canvas.height; y++){
-      pixels[x + y * canvas.width] = new Pixel;
-    }
-  }
-}
-
-function setPixelColor(x, y, r, g, b) {
-  pixels[x + y * canvas.width].colorR = r;
-  pixels[x + y * canvas.width].colorG = g;
-  pixels[x + y * canvas.width].colorB = b;
-}
-
-function drawPixel(x, y) {
-  ctx.fillStyle = "rgb(" + pixels[x + y * canvas.width].colorR + ", " + pixels[x + y * canvas.width].colorG + ", " + pixels[x + y * canvas.width].colorB + ")"
-  ctx.fillRect(x, y, 1, 1);
-}
+var metaPoints = [];
 
 function MetaPoint() {
   this.x = random(0, canvas.width);
@@ -43,17 +13,11 @@ function MetaPoint() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for(x = 0; x < canvas.width; x++){
-    for(y = 0; y < canvas.height; y++){
-      drawPixel(x, y);
-    }
-  }
 }
 
 function start() {
-  makePixels();
-  setInterval(draw, 100/3);
-  var metaPoints
+  var metaPoints[1] = new MetaPoint;
+  setInterval(draw, 10);
 }
 
 start();
