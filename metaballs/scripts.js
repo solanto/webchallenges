99ef8885ea95506e8ec6069bc5bed73b973eab65
solanto <solanto@users.noreply.github.com>
2016-12-11 21:43:32 -0800

    Updated scripts.js

--- metaballs/scripts.js
+++ metaballs/scripts.js
@@ -5,26 +5,9 @@ canvas.width = canvasWidthFromCSS.substring(0, canvasWidthFromCSS.length - 2);
 var canvasHeightFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('height');
 canvas.height = canvasHeightFromCSS.substring(0, canvasHeightFromCSS.length - 2);
 var metaPoints = [];
-var mousePos;
-
-function random(min, max) {
-  return Math.floor((Math.random() * max) + min);
-}
-
-function getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-}
-
-canvas.addEventListener('mousemove', function(evt) {
-  var mousePos = getMousePos(canvas, evt);
-}, false);
 
 function MetaPoint() {
-  this.x = mousePos.x;
+  this.x = random(0, canvas.width);
   this.y = random(0, canvas.height);
 }
 
@@ -33,7 +16,7 @@ function draw() {
 }
 
 function start() {
-  metaPoints[1] = new MetaPoint;
+  var metaPoints[1] = new MetaPoint;
   setInterval(draw, 10);
 }
 
