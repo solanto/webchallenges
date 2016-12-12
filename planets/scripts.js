7dee946a49099b1c854aacd6ab2469aa3a638528
solanto <solanto@users.noreply.github.com>
2016-12-12 10:49:06 -0800

    Updated scripts.js

--- planets/scripts.js
+++ planets/scripts.js
@@ -1,1 +1,21 @@
+var motionBlurAmount = 0.2;
 
+var canvas = document.getElementById("sky");
+var ctx = canvas.getContext("2d");
+var backgroundOpacity = 1 - motionBlurAmount;
+var backgroundColor = window.getComputedStyle(canvas, null).getPropertyValue('background-color');
+var canvasWidthFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('width');
+canvas.width = canvasWidthFromCSS.substring(0, canvasWidthFromCSS.length - 2);
+var canvasHeightFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('height');
+canvas.height = canvasHeightFromCSS.substring(0, canvasHeightFromCSS.length - 2);
+
+function random(min, max) {
+  return Math.floor((Math.random() * max) + min);
+}
+
+function clearFrameWithMotionBlur() {
+  ctx.globalAlpha = backgroundOpacity;
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.globalAlpha = 1;
+}
