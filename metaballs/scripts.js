1ec01b990ab3fb46858a41527d2c4e9647328d32
solanto <solanto@users.noreply.github.com>
2016-12-11 21:32:42 -0800

    Updated scripts.js

--- metaballs/scripts.js
+++ metaballs/scripts.js
@@ -6,6 +6,18 @@ var canvasHeightFromCSS = window.getComputedStyle(canvas, null).getPropertyValue
 canvas.height = canvasHeightFromCSS.substring(0, canvasHeightFromCSS.length - 2);
 var metaPoints = [];
 
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
+canvas.addEventListener('mousemove', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+}, false);
+
 function MetaPoint() {
   this.x = random(0, canvas.width);
   this.y = random(0, canvas.height);
