89dc05ba64a1aa3feb92998b593509d958bb461c
solanto <solanto@users.noreply.github.com>
2016-12-11 18:37:16 -0800

    Updated scripts.js

--- metaballs/scripts.js
+++ metaballs/scripts.js
@@ -1,1 +1,7 @@
+var canvas = document.getElementById("sky");
+var ctx = canvas.getContext("2d");
+var canvasWidthFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('width');
+canvas.width = canvasWidthFromCSS.substring(0, canvasWidthFromCSS.length - 2);
+var canvasHeightFromCSS = window.getComputedStyle(canvas, null).getPropertyValue('height');
+canvas.height = canvasHeightFromCSS.substring(0, canvasHeightFromCSS.length - 2);
 
